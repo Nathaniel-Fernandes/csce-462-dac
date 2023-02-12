a4b4b41e282042bf4a51d9375d645a8d271e0e49
Nathaniel <njfernandes24@tamu.edu>
2023-02-12 15:25:12 -0600

    sin wave

--- lab2.py
+++ lab2.py
@@ -10,14 +10,20 @@ GPIO.setmode(GPIO.BOARD)
 i2c = busio.I2C(board.SCL, board.SDA)
 dac = adafruit_mcp4725.MCP4725(i2c)
 
-def sin_wave():
+def sin_wave(freq, max_voltage):
     t = 0.0
     tStep = 0.05
     while True:
-        voltage = 2048*(1.0+0.5*math.sin(6.2832*t))
+        voltage = 2048 * (1.0+0.5*math.sin(2*math.pi*freq*t))
         dac.set_voltage(int(voltage))
         t += tStep
-        time.sleep(0.0005)
+
+        # button press
+        if GPIO.input(40):
+            return
+        time.sleep(0.0005) # off by 0.0005 each time
+
+            
 
 def main():
     GPIO.setup(40, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
@@ -31,12 +37,8 @@ def main():
         frequency = input("What's the frequency?")
         max_vout = input("What's the max output voltage?")
 
-        while True:
-            
-            if shape == "sin":
-                sin_wave()
+        if shape == "sin":
+            sin_wave()
                 
-            # button press
-            if GPIO.input(40):
-                break
+            
 
